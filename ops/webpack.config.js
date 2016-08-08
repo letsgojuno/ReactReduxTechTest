a9e0b52ab84862882ac6e6011e4def042a76a22f
Michael Mathews <michael.mathews@rehabstudio.com>
2016-08-08 15:34:01 +0100

    Save

--- ops/webpack.config.js
+++ ops/webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     entry: './src/index.js',
     output: {
         path: './dist',
-        filename: '[name].js'
+        filename: 'index.js'
     },
     module: {
         loaders: [{
